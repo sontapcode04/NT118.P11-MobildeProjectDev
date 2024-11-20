d8c38d1f07f02b5b10b4bcb17fd967d4ec198a80
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-11-20 11:30:31 +0700

    intent link activity

---
+++ app/src/main/java/com/example/mobileproject/SignUp.java
@@ -0,0 +1,12 @@
+package com.example.mobileproject;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup_activity);
+    }
+}
+
--- app/src/main/java/com/example/mobileproject/Signin.java
+++ app/src/main/java/com/example/mobileproject/Signin.java
@@ -1,19 +1,25 @@
 package com.example.mobileproject;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class Signin extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.dashboard);
+        setContentView(R.layout.signin_activity);
+
+        TextView tvSignUp = findViewById(R.id.sign_up);
+        tvSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Signin.this, SignUp.class);
+                startActivity(intent);
+            }
+        });
     }
 }
