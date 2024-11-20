41b331224fab9e1a5a66a0f7a39fbd8674181bd6
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-11-20 11:56:52 +0700

    add intent pothole detail

---
+++ app/src/main/java/com/example/mobileproject/dashboard.java
@@ -0,0 +1,13 @@
+package com.example.mobileproject;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class dashboard extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+}
