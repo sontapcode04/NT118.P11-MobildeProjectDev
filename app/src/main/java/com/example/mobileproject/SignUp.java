c8d00dfae358f444660faed9b998f92a8e4f8bb5
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-12-02 21:09:50 +0700

    them intent cho layout

--- app/src/main/java/com/example/mobileproject/SignUp.java
+++ app/src/main/java/com/example/mobileproject/SignUp.java
@@ -3,6 +3,8 @@ package com.example.mobileproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,14 @@ public class SignUp extends AppCompatActivity {
             }
         });
 
+        ImageView back = findViewById(R.id.backToSignIn);
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUp.this, Signin.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
