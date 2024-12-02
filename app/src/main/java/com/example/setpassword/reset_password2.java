063889ab2a40766c5fa326b6042677dfbfa7c4e8
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-12-02 21:32:14 +0700

    tao nhanh

---
+++ app/src/main/java/com/example/setpassword/reset_password2.java
@@ -0,0 +1,29 @@
+package com.example.setpassword;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class reset_password2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_reset_password2);
+
+        ImageView backButton = findViewById(R.id.back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
