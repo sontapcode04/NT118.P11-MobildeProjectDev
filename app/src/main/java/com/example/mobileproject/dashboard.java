4b26328f09b95c98661a36417c160a14c1b0d8a6
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-12-10 15:38:20 +0700

    add intent to dashboard and setting

--- app/src/main/java/com/example/mobileproject/dashboard.java
+++ app/src/main/java/com/example/mobileproject/dashboard.java
@@ -1,5 +1,8 @@
 package com.example.mobileproject;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +11,14 @@ public class dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard);
+
+        ImageView setting = findViewById(R.id.imageViewSetting);
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(dashboard.this, setting.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/mobileproject/setting.java
+++ app/src/main/java/com/example/mobileproject/setting.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -43,7 +44,14 @@ public class setting extends AppCompatActivity {
             }
         });
 
-
+        ImageView home = findViewById(R.id.imageViewHome);
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(setting.this, dashboard.class);
+                startActivity(intent);
+            }
+        });
 
 
         // Xử lý sự kiện khi nhấn vào profileLayout
