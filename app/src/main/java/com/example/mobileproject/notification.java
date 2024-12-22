821e56f4e492c4517a842d9e82dd0ba485b44333
ThinhQuang08 <145360383+ThinhQuang08@users.noreply.github.com>
2024-12-22 13:27:42 +0700

    add intent layout

--- app/src/main/java/com/example/mobileproject/notification.java
+++ app/src/main/java/com/example/mobileproject/notification.java
@@ -2,6 +2,8 @@ package com.example.mobileproject;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +19,15 @@ public class notification extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification);
+
+        //back button
+        ImageView backButton = findViewById(R.id.backButton);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
         RecyclerView recyclerView = findViewById(R.id.ListNoti);
 
